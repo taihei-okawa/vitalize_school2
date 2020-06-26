ad41bf086b4619b6352f40bd2b1716fa609e0bdc
hiroki-kuwahara0131 <hiroki.kuwahara@vitalize.co.jp>
2020-06-26 13:44:08 +0900

    mst_auth検索機能

--- src/main/java/com/example/demo/repository/MstAuthRepository.java
+++ src/main/java/com/example/demo/repository/MstAuthRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.MstUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,4 +15,6 @@ import com.example.demo.entity.MstAuth;
  */
 
 @Repository
-public interface MstAuthRepository extends JpaRepository<MstAuth, Long> {}
+public interface MstAuthRepository extends JpaRepository<MstAuth, Long>, JpaSpecificationExecutor<MstAuth> {
+    public Page<MstAuth> findAll(Pageable pageable);
+}
--- src/main/java/com/example/demo/service/MstAuthService.java
+++ src/main/java/com/example/demo/service/MstAuthService.java
@@ -1,14 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.MstAuth;
+import com.example.demo.entity.MstUser;
 import com.example.demo.repository.MstAuthRepository;
+import com.example.demo.searchform.MstAuthSearchForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 権限一覧情報 Service
@@ -35,7 +39,31 @@ public class MstAuthService {
   /**
    * 取引履歴機能の内容とページネーションを全検索
    */
-  public Page<MstAuth> getAll(Pageable pageable) {
-    return mstAuthRepository.findAll(pageable);
+  public Page<MstAuth> getAll(Pageable pageable, MstAuthSearchForm searchForm) {
+    Specification<MstAuth> spec = Specification
+            .where(authIdEqual(searchForm.getId()))
+            .and(statusEqual(searchForm.getStatus()));
+    return mstAuthRepository.findAll(spec, pageable);
+  }
+
+
+  /**
+   *  ID検索
+   */
+  private static Specification<MstAuth> authIdEqual(String id) {
+    // ラムダ式で記述すると、引数のデータ型の指定が省略できる
+    return id == "" || Objects.isNull(id) ? null : (root, query, cb) -> {
+      return cb.equal(root.get("id"),  id);
+    };
+  }
+
+  /**
+   *  ユーザー名検索
+   */
+  private static Specification<MstAuth> statusEqual(String status) {
+    // ラムダ式で記述すると、引数のデータ型の指定が省略できる
+    return status == "" || Objects.isNull(status) ? null : (root, query, cb) -> {
+      return cb.equal(root.get("status"), status);
+    };
   }
 }
